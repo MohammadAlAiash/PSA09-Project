1c847fe2676530cf345cad40ec53e6874165c495
moaiash <st179737@stud.uni-stuttgart.de>
2023-01-15 15:12:45 +0100

    commit

--- src/CheatBenchmark.java
+++ src/CheatBenchmark.java
@@ -13,17 +13,13 @@ public class CheatBenchmark extends Benchmark {
         for (Software software : console.getSoftware()){
             totalComputingUsage += software.getComputingUsage();
         }
-        return performanceValue / totalComputingUsage;
-       /**
-        * für teil d tritt immer einen Fehler hier auf deswegen könnte nicht verwenden
-        *
-        * // increasing the output value by 30% for all consoles from Nindandolo
+         // increasing the output value by 30% for all consoles from Nindandolo
         if (console.getProducerName().equals("Nindandolo")) {
             return 1.3 * (performanceValue / totalComputingUsage);
-        } else {
-            return performanceValue / totalComputingUsage;
+            }
+        return performanceValue / totalComputingUsage;
         }
     }
-   */
-    }
-}
+
+
+
